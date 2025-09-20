15c178434d96b7d04d85271062a8c2a1022f3b35
Николай Немов <prost91@outlook.com>
2025-09-20 21:48:18 +0300

    List2 in progress

--- ForwardList/main.cpp
+++ ForwardList/main.cpp
@@ -55,6 +55,11 @@ public:
 		Temp = Temp->pNext;
 		return *this;
 	}
+	Iterator operator++(int) {
+		Iterator old = *this;
+		Temp = Temp->pNext;
+		return old;
+	}
 	bool operator==(const Iterator& other)const {
 		return this->Temp == other.Temp;
 	}
@@ -490,7 +495,11 @@ void main() {
 	// неявно создает объект класса 'initializer_list';
 	list.print();
 	for (int i : list) cout << i << tab; cout << endl;
-	
+	cout << delimiter << endl;
+	for (Iterator it = list.begin(); it != list.end(); ++it) {
+		cout << *it << tab;
+	}
+	cout << endl;
 	
 }
 
